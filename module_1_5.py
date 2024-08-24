34c74f2e5f9a9e028db3854e96983cfb97cb7a20
Viktor757 <vitek-594@yandex.ru>
2024-08-24 19:20:55 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (7, 'frost', False)
+print(immutable_var)
+#immutable_var_[1] = 'viper'
+#print(immutable_var) данный кортеж не может обращаться по элементам
+mutable_list = ["BMW","Audi","MERSEDES"]
+mutable_list [2]='ferrari'
+print(mutable_list)
